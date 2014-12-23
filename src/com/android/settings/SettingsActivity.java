5e21b8ec950fe99211a5b2605270abf4e21530b6
Janson Kang <temasek71@gmail.com>
2014-12-23 23:15:00 +0800

    Intergrate Viper4Android into settings
    Conflicts:
    
    	res/xml/dashboard_categories.xml
    	src/com/android/settings/SettingsActivity.java

--- src/com/android/settings/SettingsActivity.java
+++ src/com/android/settings/SettingsActivity.java
@@ -1261,6 +1261,16 @@ public class SettingsActivity extends Activity
                             !(pm.hasPowerProfiles() || (showDev && !Build.TYPE.equals("user")))) {
                         removeTile = true;
                     }
+                } else if (id == R.id.viper_settings) {
+                    // Embedding into Settings only if app exists (user could manually remove it)
+                    boolean supported = false;
+                    try {
+                        supported = (getPackageManager().getPackageInfo("com.vipercn.viper4android_v2", 0).versionCode >= 18);
+                    } catch (PackageManager.NameNotFoundException e) {
+                    }
+                    if (!supported) {
+                        removeTile = true;
+                    }
                 }
 
                 if (UserHandle.MU_ENABLED && UserHandle.myUserId() != 0
