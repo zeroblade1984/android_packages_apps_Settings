eb8a5d118ca870523bd042b90201fc0b062f0f16
yank555-lu <yank555.lu@gmail.com>
2015-01-11 21:30:30 +0100

    DeviceInfo: remove unused variable in getMemInfo()
    Change-Id: Ic4275d7f5168383d051ca499d0dd8a83ab26b3b0
    Signed-off-by: yank555-lu <yank555.lu@gmail.com>

--- src/com/android/settings/DeviceInfoSettings.java
+++ src/com/android/settings/DeviceInfoSettings.java
@@ -546,7 +546,6 @@ public class DeviceInfoSettings extends SettingsPreferenceFragment implements In
 
     private String getMemInfo() {
         String result = null;
-        BufferedReader reader = null;
 
         try {
             /* /proc/meminfo entries follow this format:
