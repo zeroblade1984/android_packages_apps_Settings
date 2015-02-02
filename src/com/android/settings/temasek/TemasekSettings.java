b1fc1de2e2e798dc36d9f8baa76f5267f569fe1c
Joey Rizzoli <io.nolinuxnoparty@gmail.com>
2015-02-02 07:43:33 -0200

    Settings: HeadsUp options [2/2]
    Includes:
    
    * Settings: forward port HeadsUp options [2/2]
    
    -Global on/off
    -Blacklist and Do not disturb
    
    (by Adnan Begovic)
    
    * HeadsUp settings: fix enabled state on clean install
    
    (by Altaf Patel)
    
    * My changes
    
    - Swype action selector (delete/hide)
    
    Change-Id: Ia2ecc52034f04b11590abd62c5119f736985450a
    
    Modified to temasek
    
    Conflicts:
    	AndroidManifest.xml

--- src/com/android/settings/temasek/TemasekSettings.java
+++ src/com/android/settings/temasek/TemasekSettings.java
@@ -20,11 +20,17 @@ public class TemasekSettings extends SettingsPreferenceFragment implements
     public void onCreate(Bundle icicle) {
         super.onCreate(icicle);
         addPreferencesFromResource(R.xml.temasek_settings);
+
+        mHeadsUp = findPreference(Settings.System.HEADS_UP_NOTIFICATION);
     }
 
     @Override
     public void onResume() {
         super.onResume();
+        boolean headsUpEnabled = Settings.System.getInt(
+                getContentResolver(), Settings.System.HEADS_UP_NOTIFICATION,1) != 0;
+        mHeadsUp.setSummary(headsUpEnabled
+                ? R.string.summary_heads_up_enabled : R.string.summary_heads_up_disabled);
     }
 
     public boolean onPreferenceChange(Preference preference, Object objValue) {
