03da5113b52cfb2f649c4ea151fc4548474afe2b
Beeko <bcobine@gmail.com>
2015-02-25 23:27:31 -0700

    Add USB Tether Tile (1/2)
    Change-Id: I44c0fe278d5e1db85f450cac38b9cdf733eb5d08
    
    Conflicts:
    
    	res/values/cm_strings.xml
    	src/com/android/settings/cyanogenmod/qs/QSTileHolder.java

--- src/com/android/settings/cyanogenmod/qs/QSTileHolder.java
+++ src/com/android/settings/cyanogenmod/qs/QSTileHolder.java
@@ -185,6 +185,10 @@ public class QSTileHolder {
                 resourceName = "ic_qs_reboot";
                 stringId = R.string.qs_tile_reboot;
                 break;
+            case QSConstants.TILE_USB_TETHER:
+                resourceName = "ic_qs_usb_tether_on";
+                stringId = R.string.qs_usb_tether_tile;
+                break;
             default:
                 return null;
         }
