2a157b07191f81391d8a9cea13ce29729aa4813d
Janson Kang <temasek71@gmail.com>
2015-04-06 13:03:14 +0800

    Revert "QS: add Ambient display tile"
    FWB Not ready. LOL :D
    
    This reverts commit 8d54798ffdd3d02dff66db2a523f8417d8a58db5.

--- src/com/android/settings/SettingsActivity.java
+++ src/com/android/settings/SettingsActivity.java
@@ -114,7 +114,6 @@ import com.android.settings.notification.ZenModeSettings;
 import com.android.settings.print.PrintJobSettingsFragment;
 import com.android.settings.print.PrintSettingsFragment;
 import com.android.settings.sim.SimSettings;
-import com.android.settings.temasek.ambientdisplay.AmbientSettings;
 import com.android.settings.tts.TextToSpeechSettings;
 import com.android.settings.users.UserSettings;
 import com.android.settings.voice.VoiceInputSettings;
@@ -329,8 +328,7 @@ public class SettingsActivity extends Activity
             LiveDisplay.class.getName(),
             QSTiles.class.getName(),
             DisplayRotation.class.getName(),
-            ExpandedDesktopPreferenceFragment.class.getName(),
-            AmbientSettings.class.getName()
+            ExpandedDesktopPreferenceFragment.class.getName()
     };
 
 
