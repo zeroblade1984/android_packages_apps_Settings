052a6d494c573af9113fecd9743400429af8cf24
Janson Kang <temasek71@gmail.com>
2015-06-07 08:30:28 +0800

    QS: Clean up heads up tile (2/2)

--- src/com/android/settings/cyanogenmod/qs/QSTileHolder.java
+++ src/com/android/settings/cyanogenmod/qs/QSTileHolder.java
@@ -203,7 +203,7 @@ public class QSTileHolder {
                 break;
 	    case QSConstants.TILE_HEADS_UP:
                 resourceName = "ic_qs_heads_up_on";
-                stringId = R.string.qs_tile_headsup;
+                stringId = R.string.qs_tile_heads_up;
                 break;
             case QSConstants.TILE_TRDS:
                 resourceName = "ic_qs_trds_on";
