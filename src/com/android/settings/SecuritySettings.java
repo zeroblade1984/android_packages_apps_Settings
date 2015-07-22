kn8wolf/packages_apps_Settings

SecuritySettings: Lockscreen wallpaper
Requirements:
* Add https://github.com/SlimSaber/packages_apps_WallpaperPicker in manifest
* Add WallpaperPicker in vendor's config/common.mk

Change-Id: I4e5ddf12628902b82f8109d51ee4bcf4afbbb0f4

src/com/android/settings/SecuritySettings.java
@@ -28,6 +28,7 @@ import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.content.pm.PackageManager.NameNotFoundException;
 import android.content.pm.ResolveInfo;
 import android.content.pm.UserInfo;
 import android.content.res.Resources;
@@ -37,6 +38,7 @@ import android.os.UserManager;
 import android.preference.SwitchPreference;
 import android.preference.ListPreference;
 import android.preference.Preference;
+import android.preference.PreferenceCategory;
 import android.preference.Preference.OnPreferenceChangeListener;
 import android.preference.PreferenceGroup;
 import android.preference.PreferenceScreen;
@@ -92,6 +94,8 @@ public class SecuritySettings extends SettingsPreferenceFragment
     private static final String LOCKSCREEN_QUICK_UNLOCK_CONTROL = "quick_unlock_control";
     private static final String LOCK_NUMPAD_RANDOM = "lock_numpad_random";
     private static final String LOCKSCREEN_BOTTOM_SHORTCUTS = "lockscreen_bottom_shortcuts";
+    private static final String KEY_LOCKSCREEN_CATEGORY = "lockscreen_category";
+    private static final String KEY_LOCKSCREEN_WALLPAPER = "lockscreen_wallpaper";
 
     private static final int SET_OR_CHANGE_LOCK_METHOD_REQUEST = 123;
     private static final int CONFIRM_EXISTING_FOR_BIOMETRIC_WEAK_IMPROVE_REQUEST = 124;
@@ -487,6 +491,17 @@ public class SecuritySettings extends SettingsPreferenceFragment
             PreferenceGroup appCategory = (PreferenceGroup)
                     root.findPreference(KEY_APP_SECURITY_CATEGORY);
             appCategory.removePreference(mBlacklist);
+        }
+
+        // Lockscreen wallpaper
+        PreferenceCategory lockscreenCategory = (PreferenceCategory)
+            root.findPreference(KEY_LOCKSCREEN_CATEGORY);
+        PreferenceScreen lockscreenWallpaper = (PreferenceScreen)
+            lockscreenCategory.findPreference(KEY_LOCKSCREEN_WALLPAPER);
+        try {
+            getActivity().getPackageManager().getPackageInfo("com.slim.wallpaperpicker", 0);
+        } catch (PackageManager.NameNotFoundException e) {
+            lockscreenCategory.removePreference(lockscreenWallpaper);
         }
 
         // The above preferences come and go based on security state, so we need to update

zeroblade1984/android_packages_apps_Settings

src/com/android/settings/SecuritySettings.java
@@ -35,6 +35,7 @@ import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.content.pm.PackageManager.NameNotFoundException;
 import android.content.pm.ResolveInfo;
 import android.content.pm.UserInfo;
 import android.content.res.Resources;
@@ -46,6 +47,7 @@ import android.os.UserManager;
 import android.preference.SwitchPreference;
 import android.preference.ListPreference;
 import android.preference.Preference;
+import android.preference.PreferenceCategory;
 import android.preference.Preference.OnPreferenceChangeListener;
 import android.preference.PreferenceGroup;
 import android.preference.PreferenceScreen;
@@ -96,6 +98,8 @@ public class SecuritySettings extends SettingsPreferenceFragment
     private static final String KEY_CREDENTIALS_MANAGER = "credentials_management";
     private static final String PACKAGE_MIME_TYPE = "application/vnd.android.package-archive";
     private static final String KEY_SCREEN_PINNING = "screen_pinning_settings";
+    private static final String KEY_GENERAL_CATEGORY = "general_category";
+    private static final String KEY_LOCKSCREEN_WALLPAPER = "lockscreen_wallpaper";
 
     // Cyanogen device lock
     public static final String ACCOUNT_TYPE_CYANOGEN = "com.cyanogen";
@@ -301,6 +305,17 @@ public class SecuritySettings extends SettingsPreferenceFragment
             if (deviceAdminCategory != null) {
                 deviceAdminCategory.removePreference(appOpsSummary);
             }
+        }
+
+        // Lockscreen wallpaper
+        PreferenceCategory generalCategory = (PreferenceCategory)
+            root.findPreference(KEY_GENERAL_CATEGORY);
+        PreferenceScreen lockscreenWallpaper = (PreferenceScreen)
+            generalCategory.findPreference(KEY_LOCKSCREEN_WALLPAPER);
+        try {
+            getActivity().getPackageManager().getPackageInfo("com.slim.wallpaperpicker", 0);
+        } catch (PackageManager.NameNotFoundException e) {
+            generalCategory.removePreference(lockscreenWallpaper);
         }
 
         // The above preferences come and go based on security state, so we need to update