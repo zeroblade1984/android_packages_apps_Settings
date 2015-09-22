ed1bcc09fbd176c50bd0cc34f7b9d5d60e377c28
Janson Kang <temasek71@gmail.com>
2015-09-22 18:13:00 +0800

    Settings: Add option for "modern" custom headers (2/2)
    Signed-off-by: Janson Kang <temasek71@gmail.com>

--- src/com/android/settings/cyanogenmod/NotificationDrawerSettings.java
+++ src/com/android/settings/cyanogenmod/NotificationDrawerSettings.java
@@ -55,7 +55,7 @@ public class NotificationDrawerSettings extends SettingsPreferenceFragment imple
     private Preference mQSTiles;
     private ListPreference mNumColumns;
     private SwitchPreference mCustomHeader;
-    private SwitchPreference mCustomHeaderDefault;
+    private ListPreference mCustomHeaderDefault;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -118,10 +118,11 @@ public class NotificationDrawerSettings extends SettingsPreferenceFragment imple
         mCustomHeader.setOnPreferenceChangeListener(this);
 
         // Status bar custom header default
-        mCustomHeaderDefault = (SwitchPreference) prefSet.findPreference(PREF_CUSTOM_HEADER_DEFAULT);
-        mCustomHeaderDefault.setChecked((Settings.System.getInt(getActivity().getContentResolver(),
-                Settings.System.STATUS_BAR_CUSTOM_HEADER_DEFAULT, 0) == 1));
+        mCustomHeaderDefault = (ListPreference) findPreference(PREF_CUSTOM_HEADER_DEFAULT);
         mCustomHeaderDefault.setOnPreferenceChangeListener(this);
+        int customHeaderDefault = Settings.System.getInt(getContentResolver(),
+                Settings.System.STATUS_BAR_CUSTOM_HEADER_DEFAULT, 0);
+        mCustomHeaderDefault.setValue(String.valueOf(customHeaderDefault));
     }
 
     @Override
@@ -166,9 +167,10 @@ public class NotificationDrawerSettings extends SettingsPreferenceFragment imple
                     (Boolean) newValue ? 1 : 0);
             return true;
         } else if (preference == mCustomHeaderDefault) {
-            Settings.System.putInt(getContentResolver(),
+            int customHeaderDefault = Integer.valueOf((String) newValue);
+            Settings.System.putIntForUser(getContentResolver(), 
                     Settings.System.STATUS_BAR_CUSTOM_HEADER_DEFAULT,
-                    (Boolean) newValue ? 1 : 0);
+                    customHeaderDefault, UserHandle.USER_CURRENT);
             Settings.System.putInt(getContentResolver(),
                     Settings.System.STATUS_BAR_CUSTOM_HEADER,
                     0);
